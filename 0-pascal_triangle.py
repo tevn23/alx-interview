43e63654da9ce2fe9f63d9a8a2a0592ee4f07266
tevn23 <mabulatt23@gmail.com>
2024-10-04 06:00:09 +0200

    Reimplementation

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangl
-"""
+"""Pascal's Triangle"""
 
 
 def pascal_triangle(n):
     """
-    Implementing the pascal triangle
+    a function that returns a list
+    of integers representing the
+    pascal triangle of n:
+       . Returns an empty list if n <= 0
+       . assume n will be always an integer
     """
+    pascal_tri = []
 
     if n <= 0:
         return []
-    
-    triangle = [[1]]  # Initialize the first row of Pascal's triangle
-    
-    for i in range(1, n):
-        # Create the next row
-        row = [1]  # First element of the row is always 1
-        # Compute the middle elements
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        # Last element of the row is always 1
-        row.append(1)
-        triangle.append(row)
-    
-    return triangle
+
+    for i in range(n):
+        if (i == 0):
+            pascal_tri.append([1])
+        else:
+            cur_row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(pascal_tri[i - 1][j - 1] +
+                                   pascal_tri[i - 1][j])
+
+            pascal_tri.append(cur_row)
+
+    return (pascal_tri)

2dcdaec5872585aaf50c2ff4608001220662eba4
tevn23 <mabulatt23@gmail.com>
2024-10-09 03:02:50 +0200

    Implemented method to check open lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Implemeting a method that determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all boxes can be unlocked"""
+    # Keep track of unlocked boxes with a set
+    unlocked = {0}
+    
+    # Stack to process boxes and collect new keys
+    keys = [0]
+    
+    while keys:
+        # Get a box from the stack
+        current_box = keys.pop()
+        
+        # Try to unlock new boxes with the keys from the current box
+        for key in boxes[current_box]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.add(key)
+                keys.append(key)
+    
+    # If the number of unlocked boxes equals the total number of boxes, return True
+    return len(unlocked) == len(boxes)
